bd15f18ba7380c9ed5370bbac3d6b0a54568d355
GitTheHubOutOfHere <david.coding1358@gmail.com>
2025-04-11 15:44:54 +1000

    Added non-functional 'idcard' item

--- src/main/java/com/dave08/idcards/IDCards.java
+++ src/main/java/com/dave08/idcards/IDCards.java
@@ -1,6 +1,9 @@
 package com.dave08.idcards;
 
+import com.dave08.idcards.item.ModItems;
+import com.dave08.idcards.item.ModTabs;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -27,6 +30,9 @@ public class IDCards
     {
         IEventBus modEventBus = context.getModEventBus();
 
+        ModTabs.register(modEventBus);
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -48,7 +54,10 @@ public class IDCards
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        /*if(event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.IDCARD);
+        }/**/
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/com/dave08/idcards/item/ModTabs.java
@@ -0,0 +1,31 @@
+package com.dave08.idcards.item;
+
+import com.dave08.idcards.IDCards;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModTabs
+{
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, IDCards.MOD_ID);
+
+    //Add a creative mod tab called "ID Cards" (en_us.json creativetab.idcards_tab)
+    public static final RegistryObject<CreativeModeTab> IDCARD_TAB = CREATIVE_MODE_TABS.register("idcard_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.IDCARD.get()))
+                    .title(Component.translatable("creativetab.idcards_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.IDCARD.get());
+                        //output.accept(Items.DIAMOND); For adding vannilla items
+                    }).build());
+
+    public static void register(IEventBus eventBus)
+    {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
