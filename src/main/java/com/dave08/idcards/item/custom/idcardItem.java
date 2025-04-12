dd89bb241866d21285266b1d3c2e3595faa7841c
GitTheHubOutOfHere <david.coding1358@gmail.com>
2025-04-12 15:24:16 +1000

    Added controls for item 'idcard' and included all files in github repo

--- src/main/java/com/dave08/idcards/item/custom/idcardItem.java
+++ src/main/java/com/dave08/idcards/item/custom/idcardItem.java
@@ -1,6 +1,7 @@
 package com.dave08.idcards.item.custom;
 
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
@@ -11,6 +12,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -30,15 +33,34 @@ public class idcardItem extends Item
         CompoundTag tag = stack.getOrCreateTag();
         if (tag.contains("OwnerUUID", Tag.TAG_INT_ARRAY)) {
             UUID storedUuid = tag.getUUID("OwnerUUID");
-            if (storedUuid.equals(pPlayer.getUUID())) {
-                // Player is the owner — clear the tag
-                stack.setTag(null); // or stack.removeTagKey("OwnerUUID"); + remove other keys individually
+
+            // Disallow non-owners to clear ownership
+            if (!storedUuid.equals(pPlayer.getUUID()))
+            {
                 if (!pLevel.isClientSide) {
-                    pPlayer.displayClientMessage(Component.translatable("item.idcards.idcard.actionBarMessageUnlinked")
+                    pPlayer.displayClientMessage(Component.translatable("item.idcards.idcard.actionBarMessageNotOwner")
                             .withStyle(ChatFormatting.RED), true);
                 }
-                return InteractionResultHolder.sidedSuccess(stack, pLevel.isClientSide); // REQUIRED - Will not clear owner without
+                return InteractionResultHolder.sidedSuccess(stack, pLevel.isClientSide);
             }
+
+            // Player must crouch to clear data
+            if (!pPlayer.isCrouching())
+            {
+                if (!pLevel.isClientSide) {
+                    pPlayer.displayClientMessage(Component.translatable("item.idcards.idcard.actionBarMessageAlreadyLinked")
+                            .withStyle(ChatFormatting.WHITE), true);
+                }
+                return InteractionResultHolder.sidedSuccess(stack, pLevel.isClientSide);
+            }
+
+            // Player is the owner — clear the data
+            stack.setTag(null); // or stack.removeTagKey("OwnerUUID"); + remove other keys individually
+            if (!pLevel.isClientSide) {
+                pPlayer.displayClientMessage(Component.translatable("item.idcards.idcard.actionBarMessageUnlinked")
+                        .withStyle(ChatFormatting.RED), true);
+            }
+            return InteractionResultHolder.sidedSuccess(stack, pLevel.isClientSide);
         }
 
         // Else: set ownership
@@ -56,11 +78,22 @@ public class idcardItem extends Item
     }
 
     @Override
+    @OnlyIn(Dist.CLIENT)
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
-        tooltip.add(Component.translatable("item.idcards.idcard.tooltip.base"));
+        tooltip.add(Component.translatable("item.idcards.idcard.tooltip.base1"));
+        tooltip.add(Component.literal(""));
+        tooltip.add(Component.literal("[Ctrl] for controls"));
+
+        // Show controls when control key is pressed
+        if (Screen.hasControlDown())
+        {
+            tooltip.add(Component.translatable("item.idcards.idcard.tooltip.base2"));
+            tooltip.add(Component.translatable("item.idcards.idcard.tooltip.base3"));
+        }
 
         if (stack.hasTag() && stack.getTag().contains("OwnerName", Tag.TAG_STRING)) {
             Component ownerName = Component.literal(stack.getTag().getString("OwnerName")).withStyle(ChatFormatting.GREEN);
+            tooltip.add(Component.literal(""));
             tooltip.add(Component.translatable("item.idcards.idcard.tooltip.owner", ownerName));
         }
     }
