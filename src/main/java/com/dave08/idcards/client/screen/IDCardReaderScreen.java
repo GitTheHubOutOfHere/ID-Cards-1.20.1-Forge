fff90fc2b93f60cbec0a1a148c8ef41e4230267e
David V <david.coding1358@gmail.com>
2025-05-07 08:34:22 +1000

    Revert "Added start of pulse editor"
    This reverts commit 4f4d97053bba22bc1ed91ee8fb5110751c20ed62.

--- src/main/java/com/dave08/idcards/client/screen/IDCardReaderScreen.java
+++ src/main/java/com/dave08/idcards/client/screen/IDCardReaderScreen.java
@@ -3,15 +3,13 @@ package com.dave08.idcards.client.screen;
 import com.dave08.idcards.block.entity.menu.IDCardReaderMenu;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
 public class IDCardReaderScreen extends AbstractContainerScreen<IDCardReaderMenu> {
-    private @SuppressWarnings("deprication") static final ResourceLocation TEXTURE = new ResourceLocation("idcards", "textures/gui/IdcardReaderMenu.png");
-    private EditBox pulse;
+    private @SuppressWarnings("deprication") static final ResourceLocation TEXTURE = new ResourceLocation("minecraft", "textures/gui/container/generic_54.png");
 
     public IDCardReaderScreen(IDCardReaderMenu menu, Inventory playerInv, Component title) {
         super(menu, playerInv, title);
@@ -41,44 +39,5 @@ public class IDCardReaderScreen extends AbstractContainerScreen<IDCardReaderMenu
         this.renderBackground(guiGraphics);
         super.render(guiGraphics, mouseX, mouseY, partialTick);
         this.renderTooltip(guiGraphics, mouseX, mouseY);
-        pulse.render(guiGraphics, mouseX, mouseY, partialTick);
-    }
-
-    @Override
-    protected void init() {
-        super.init();
-        int x = (this.width - this.imageWidth) / 2;
-        int y = (this.height - this.imageHeight) / 2;
-
-        pulse = new EditBox(this.font, x + this.imageWidth + 5, y + 10, 50, 20, Component.literal("Number"));
-        pulse.setMaxLength(5);
-        pulse.setFilter(s -> s.matches("\\d*")); // digits only
-        pulse.setResponder(this::onPulseChanged);
-        this.addRenderableWidget(pulse);
-    }
-
-    private void onPulseChanged(String text) {
-        if (!text.isEmpty()) {
-            try {
-                int value = Integer.parseInt(text);
-                // Do something with 'value'
-            } catch (NumberFormatException ignored) {}
-        }
-    }
-
-    @Override
-    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-        if (pulse.keyPressed(keyCode, scanCode, modifiers) || pulse.isFocused()) {
-            return true;
-        }
-        return super.keyPressed(keyCode, scanCode, modifiers);
-    }
-
-    @Override
-    public boolean charTyped(char codePoint, int modifiers) {
-        if (pulse.charTyped(codePoint, modifiers)) {
-            return true;
-        }
-        return super.charTyped(codePoint, modifiers);
     }
 }
